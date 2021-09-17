08eaad6af637afea5e2a217708b306284af64aff
robertameireles <robertameireles.r@gmail.com>
2021-09-17 22:52:31 +0100

    included slot and component

--- resources/views/site/layouts/_components/form_contact.blade.php
+++ resources/views/site/layouts/_components/form_contact.blade.php
@@ -1,22 +1,21 @@
 {{ $slot }}
-{{ $class ?? '' }}
 {{ $x ?? '' }}
 <form action={{ route('name.contact'), }} method='post'>
     @csrf
-    <input type="text" placeholder="Nome" class="borda-preta">
+    <input type="text" placeholder="Nome" class={{ $classe ?? 'borda-preta' }}>
     <br>
-    <input type="text" placeholder="Telefone" class="borda-preta">
+    <input type="text" placeholder="Telefone" class={{ $classe ?? 'borda-preta' }}>
     <br>
-    <input type="text" placeholder="E-mail" class="borda-preta">
+    <input type="text" placeholder="E-mail" class={{ $classe ?? 'borda-preta' }}>
     <br>
-    <select class="borda-preta">
+    <select class={{ $classe ?? 'borda-preta' }}>
         <option value="">Qual o motivo do contato?</option>
         <option value="">Dúvida</option>
         <option value="">Elogio</option>
         <option value="">Reclamação</option>
     </select>
     <br>
-    <textarea class="borda-preta">Preencha aqui a sua mensagem</textarea>
+    <textarea class={{ $classe ?? 'borda-preta' }}>Preencha aqui a sua mensagem</textarea>
     <br>
     <button type="submit" class="borda-preta">ENVIAR</button>
 </form>
--- resources/views/site/main.blade.php
+++ resources/views/site/main.blade.php
@@ -28,7 +28,7 @@
             <div class="contato">
                 <h1>Contato</h1>
                 <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
-                @component('site.layouts._components.form_contact')                    
+                @component('site.layouts._components.form_contact', ['classe' => 'borda-branca'])                    
                 @endcomponent
             </div>
         </div>
